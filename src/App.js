d4da80f4c9070e2403c9bc9e28597b3a2b267972
Ankitkumar327 <as077924@gmail.com>
2022-03-03 09:34:03 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./Component/Home.js";
+import Navbar from "./Component/Navbar.js";
+import Student from "./Component/Student.js";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Contactus from "./Component/Contactus.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/contact" element={<Contactus />}></Route>
+        <Route path="/student" element={<Student />}></Route>
+      </Routes>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/Component/Contactus.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const Contactus = () => {
+  return (
+    <>
+      <h1>Contact Page</h1>
+      <Outlet />
+    </>
+  );
+};
+export default Contactus;
